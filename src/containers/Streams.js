a82ff9c035cf10e8f959e6f14f34be8477e4431b
elegin <elegin@gmail.com>
2020-05-14 11:41:40 -0400

    add interval to livestreams

--- src/containers/Streams.js
+++ src/containers/Streams.js
@@ -38,6 +38,12 @@ const Streams = ({users}) => {
       // console.log(response);
       setUserData(response)
     });
+    setInterval(() => {
+     twitchUserData().then(function(response) {
+        console.log(response);
+        setUserData(response)
+      });
+    }, 10000);  
   }, []);   
 
 
